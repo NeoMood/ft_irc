0c2b5016b5b0d67b9d70154ad0be080704e7abb6
ayoub.aqlzim <ayoub.a@beyond-believers.com>
2023-05-20 13:34:00 +0100

    resolve conflicts

---
+++ includes/Channel.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <map>
+#include <vector>
+#include <stdexcept>
+#include <string>
+
+// Numeric Replies:
+
+# define ERR_NEEDMOREPARAMS
+# define ERR_BANNEDFROMCHAN
+# define ERR_INVITEONLYCHAN
+# define ERR_BADCHANNELKEY
+# define ERR_CHANNELISFULL
+# define ERR_BADCHANMASK
+# define ERR_NOSUCHCHANNEL
+# define ERR_TOOMANYCHANNELS
+# define ERR_TOOMANYTARGETS
+# define ERR_UNAVAILRESOURCE
+
+class Channel {
+
+};
